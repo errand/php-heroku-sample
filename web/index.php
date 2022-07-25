049c3b4f595b94438f1969ccd90ef9d0f73fb14b
Aleksandr Shatskikh <work@errand.ru>
2022-07-25 14:35:19 +0300

    refactor: code styling

--- web/index.php
+++ web/index.php
@@ -2,22 +2,17 @@
 
 session_start();
 ?>
-
 <html>
 <head>
-
+<title>Sample Heroku app</title>
 </head>
 <body>
 <?php
-
-if(!isset($_SESSION['username'])) {
-    include './includes/login.php';
-} else {
-    include './includes/logout.php';
-}
-
-
-
+    if(!isset($_SESSION['username'])) {
+        include './includes/login.php';
+    } else {
+        include './includes/logout.php';
+    }
 ?>
 </body>
 </html>
